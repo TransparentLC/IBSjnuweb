557f1d812605a42d89d547d6b664e1fd44206367
✨小透明・宸✨ <1272742391@qq.com>
2020-12-10 11:11:33 +0800

    Update main.php

--- main.php
+++ main.php
@@ -3,11 +3,14 @@ define('IS_PHAR', (bool)Phar::running());
 define('PHAR_PATH', IS_PHAR ? dirname(Phar::running(false)) : __DIR__);
 define('APP_PATH', IS_PHAR ? Phar::running() : __DIR__);
 define('JSON_UNESCAPED_UNICODE_SLASHES', JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+if (!defined('GIT_COMMIT_HASH')) define('GIT_COMMIT_HASH', null);
+if (!defined('GIT_COMMIT_HASH_SHORT')) define('GIT_COMMIT_HASH_SHORT', null);
+if (!defined('GIT_COMMIT_TIMESTAMP')) define('GIT_COMMIT_TIMESTAMP', null);
 
 require_once APP_PATH . '/vendor/autoload.php';
 
 if (PHP_SAPI !== 'cli') {
-    $parsed = parse_url(urldecode($_SERVER['QUERY_STRING']));
+    $parsed = parse_url(PHP_SAPI === 'cli-server' ? $_SERVER['REQUEST_URI'] : urldecode($_SERVER['QUERY_STRING']));
     $path = $parsed['path'];
     $query = isset($parsed['query']) ? $parsed['query'] : '';
     parse_str($query, $_GET);
@@ -18,14 +21,17 @@ if (PHP_SAPI !== 'cli') {
         });
     });
 
-    // if (
-    //     is_file($pathStatic = APP_PATH . "/public{$path}") ||
-    //     (preg_match_all('/\/$/', $path) && is_file($pathStatic = APP_PATH . "/public{$path}index.html"))
-    // ) {
-    //     header('Content-Type:' . mime_content_type($pathStatic));
-    //     readfile($pathStatic);
-    //     die;
-    // }
+    if (
+        PHP_SAPI === 'cli-server' &&
+        (
+            is_file($pathStatic = APP_PATH . "/public{$path}") ||
+            (preg_match_all('/\/$/', $path) && is_file($pathStatic = APP_PATH . "/public{$path}index.html"))
+        )
+    ) {
+        header('Content-Type:' . mime_content_type($pathStatic));
+        readfile($pathStatic);
+        die;
+    }
 
     $routeInfo = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], $path);
     switch ($routeInfo[0]) {
