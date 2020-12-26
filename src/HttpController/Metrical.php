069c6862c81b3e27390bcb48e47f002d15c6641f
✨小透明・宸✨ <1272742391@qq.com>
2020-12-26 14:03:41 +0800

    Update Metrical.php

--- src/HttpController/Metrical.php
+++ src/HttpController/Metrical.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\HttpController;
 
 use \App\Util;
+use \GuzzleHttp\Client;
 
 class Metrical extends \App\Component\HttpController {
     function index() {
@@ -165,97 +166,119 @@ AKARIN;
                         'x' => date($month ? 'Y-m-d' : 'Y-m', $e['time']),
                         'y' => $e['usage'],
                     ];
-                    header('Location:https://quickchart.io/chart?' . http_build_query([
-                        'w' => $month ? 1440 : 1080,
-                        'h' => 480,
-                        'f' => (strpos($_SERVER['HTTP_ACCEPT'], 'image/webp') !== false) ? 'webp' : 'png',
-                        'c' => json_encode([
-                            'type' => 'line',
-                            'options' => [
-                                'title' => [
-                                    'display' => true,
-                                    'text' => sprintf(
-                                        '%s %s耗能记录',
-                                        $room,
-                                        $month ? "{$year} 年 {$month} 月" : "{$year} 年"
-                                    ),
-                                ],
-                                'scales' => [
-                                    'xAxes' => [
-                                        [
-                                            'offset' => true,
-                                            'type' => 'time',
-                                            'time' => [
-                                                'displayFormats' => [
-                                                    'day' => 'YYYY-MM-DD',
-                                                    'month' => 'YYYY-MM',
-                                                ],
-                                                'unit' => $month ? 'day' : 'month'
+
+                    $chartData = [
+                        'type' => 'line',
+                        'options' => [
+                            'title' => [
+                                'display' => true,
+                                'text' => sprintf(
+                                    '%s %s耗能记录',
+                                    $room,
+                                    $month ? "{$year} 年 {$month} 月" : "{$year} 年"
+                                ),
+                            ],
+                            'scales' => [
+                                'xAxes' => [
+                                    [
+                                        'offset' => true,
+                                        'type' => 'time',
+                                        'time' => [
+                                            'displayFormats' => [
+                                                'day' => 'YYYY-MM-DD',
+                                                'month' => 'YYYY-MM',
                                             ],
+                                            'unit' => $month ? 'day' : 'month'
                                         ],
                                     ],
-                                    'yAxes' => [
-                                        [
-                                            'id' => 0,
-                                            'position' => 'left',
-                                            'scaleLabel' => [
-                                                'display' => true,
-                                                'labelString' => '电能（度）'
-                                            ],
-                                            'ticks' => [
-                                                'min' => 0,
-                                            ],
+                                ],
+                                'yAxes' => [
+                                    [
+                                        'id' => 0,
+                                        'position' => 'left',
+                                        'scaleLabel' => [
+                                            'display' => true,
+                                            'labelString' => '电能（度）'
                                         ],
-                                        [
-                                            'id' => 1,
-                                            'position' => 'right',
-                                            'scaleLabel' => [
-                                                'display' => true,
-                                                'labelString' => '用水量（吨）'
-                                            ],
-                                            'ticks' => [
-                                                'min' => 0,
-                                            ],
+                                        'ticks' => [
+                                            'min' => 0,
+                                        ],
+                                    ],
+                                    [
+                                        'id' => 1,
+                                        'position' => 'right',
+                                        'scaleLabel' => [
+                                            'display' => true,
+                                            'labelString' => '用水量（吨）'
+                                        ],
+                                        'ticks' => [
+                                            'min' => 0,
                                         ],
                                     ],
                                 ],
                             ],
-                            'data' => [
-                                'datasets' => [
-                                    [
-                                        'yAxisID' => 0,
-                                        'label' => '电能',
-                                        'data' => array_map($func3, $metricalData['electricity']),
-                                        'fill' => false,
-                                        'cubicInterpolationMode' => 'monotone',
-                                        'lineTension' => .5,
-                                        'borderColor' => '#4bc0c0',
-                                        'pointBackgroundColor' => '#4bc0c0',
-                                    ],
-                                    [
-                                        'yAxisID' => 1,
-                                        'label' => '冷水',
-                                        'data' => array_map($func3, $metricalData['coldWater']),
-                                        'fill' => false,
-                                        'cubicInterpolationMode' => 'monotone',
-                                        'lineTension' => .5,
-                                        'borderColor' => '#36a2eb',
-                                        'pointBackgroundColor' => '#36a2eb',
-                                    ],
-                                    [
-                                        'yAxisID' => 1,
-                                        'label' => '热水',
-                                        'data' => array_map($func3, $metricalData['hotWater']),
-                                        'fill' => false,
-                                        'cubicInterpolationMode' => 'monotone',
-                                        'lineTension' => .5,
-                                        'borderColor' => '#ff6384',
-                                        'pointBackgroundColor' => '#ff6384',
-                                    ],
+                        ],
+                        'data' => [
+                            'datasets' => [
+                                [
+                                    'yAxisID' => 0,
+                                    'label' => '电能',
+                                    'data' => array_map($func3, $metricalData['electricity']),
+                                    'fill' => false,
+                                    'cubicInterpolationMode' => 'monotone',
+                                    'lineTension' => .5,
+                                    'borderColor' => '#4bc0c0',
+                                    'pointBackgroundColor' => '#4bc0c0',
+                                ],
+                                [
+                                    'yAxisID' => 1,
+                                    'label' => '冷水',
+                                    'data' => array_map($func3, $metricalData['coldWater']),
+                                    'fill' => false,
+                                    'cubicInterpolationMode' => 'monotone',
+                                    'lineTension' => .5,
+                                    'borderColor' => '#36a2eb',
+                                    'pointBackgroundColor' => '#36a2eb',
+                                ],
+                                [
+                                    'yAxisID' => 1,
+                                    'label' => '热水',
+                                    'data' => array_map($func3, $metricalData['hotWater']),
+                                    'fill' => false,
+                                    'cubicInterpolationMode' => 'monotone',
+                                    'lineTension' => .5,
+                                    'borderColor' => '#ff6384',
+                                    'pointBackgroundColor' => '#ff6384',
                                 ],
                             ],
-                        ], JSON_UNESCAPED_UNICODE_SLASHES),
-                    ]), true, 302);
+                        ],
+                    ];
+
+                    $chartUrl = (new Client)
+                        ->post(
+                            'https://quickchart.io/chart/create',
+                            [
+                                'json' => [
+                                    'width' => $month ? 1440 : 1080,
+                                    'height' => 480,
+                                    'format' => (strpos($_SERVER['HTTP_ACCEPT'], 'image/webp') !== false) ? 'webp' : 'png',
+                                    'backgroundColor' => '#fff',
+                                    'chart' => $chartData,
+                                ],
+                            ]
+                        )
+                        ->getBody()
+                        ->getContents();
+                    $chartUrl = json_decode($chartUrl, true)['url'];
+                    header('Location:' . $chartUrl);
+
+                    // header('Location:https://quickchart.io/chart?' . http_build_query([
+                    //     'w' => $month ? 1440 : 1080,
+                    //     'h' => 480,
+                    //     'f' => (strpos($_SERVER['HTTP_ACCEPT'], 'image/webp') !== false) ? 'webp' : 'png',
+                    //     'bkg' => '#fff',
+                    //     'c' => json_encode($chartData, JSON_UNESCAPED_UNICODE_SLASHES),
+                    // ]));
                     break;
 
                 case 'json':
